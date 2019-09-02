3ae8ef8acf9af7acce4b7fa3d69221b8accdbafe
Rohit Saini <31476481+rohitsaini1196@users.noreply.github.com>
2019-09-02 19:12:45 +0530

    Add files via upload

---
+++ beer/data.js
@@ -0,0 +1,137 @@
+var data = [
+  {
+    beerName: "KingFisher",
+    beerPrice: 234,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/1.jpg",
+    beerId: 0
+  },
+  {
+    beerName: "Tuborg",
+    beerPrice: 236,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/2.jpg",
+    beerId: 1
+  },
+  {
+    beerName: "Carlsberg",
+    beerPrice: 210,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/3.jpg",
+    beerId: 2
+  },
+  {
+    beerName: "Budweiser",
+    beerPrice: 200,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/4.jpg",
+    beerId: 3
+  },
+  {
+    beerName: "Heineken",
+    beerPrice: 2,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/5.jpg",
+    beerId: 4
+  },
+  {
+    beerName: "Corona",
+    beerPrice: 372,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/6.jpg",
+    beerId: 5
+  },
+  {
+    beerName: "Bira 91",
+    beerPrice: 252,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/7.jpg",
+    beerId: 6
+  },
+  {
+    beerName: "Foster's",
+    beerPrice: 112,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/8.jpg",
+    beerId: 7
+  },
+  {
+    beerName: "Hoegaarden",
+    beerPrice: 892,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/9.jpg",
+    beerId: 8
+  },
+  {
+    beerName: "MillerCoors",
+    beerPrice: 672,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/10.jpg",
+    beerId: 9
+  },
+  {
+    beerName: "Haywards 5000",
+    beerPrice: 297,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/11.jpg",
+    beerId: 10
+  },
+  {
+    beerName: "Royal Challenge",
+    beerPrice: 250,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/12.png",
+    beerId: 11
+  },
+  {
+    beerName: "Kings",
+    beerPrice: 300,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/13.jpg",
+    beerId: 12
+  },
+  {
+    beerName: "Godfathers",
+    beerPrice: 222,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/14.jpg",
+    beerId: 13
+  },
+  {
+    beerName: "Stella Artois",
+    beerPrice: 342,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/15.jpg",
+    beerId: 14
+  },
+  {
+    beerName: "Peroni",
+    beerPrice: 455,
+    beerDiscription:
+      "welcome to index.js  and curent folder is under the viewss . im using vs code 2019 edition ... blah blah blah",
+    beerImage: "images/16.jpg",
+    beerId: 15
+  }
+];
+
+for (var i = 0; i < data.length; i++) {
+  console.log(data[i].beerName);
+  console.log(data[i].beerId);
+  console.log(data[i].beerImage);
+  console.log(data[i].beerPrice);
+}
